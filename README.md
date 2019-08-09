d33f4e0e05032eead9d6445645e6632505c7f180
KT <kt@MacBook-Pro-4.local>
2019-08-09 23:15:14 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# TEST
+# Web Programming
+## Using Flask and Material Design Lite
+
+## How to Usage
+Please move above simpler dir
+
+Then
+1. export FLASK_APP=simpler
+2. export FLASK_ENV=development
+3. flask run
---
+++ simpler/post.py
@@ -0,0 +1,73 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+from simpler.auth import login_required
+from simpler.db import get_db
+
+bp = Blueprint("post",__name__)
+
+@bp.route('/')
+def index():
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('post/index.html', posts=posts)
+
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO post (title, body, author_id)'
+                ' VALUES (?, ?, ?)',
+                (title, body, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('post.index'))
+
+    return render_template('post/create.html')
+
+def get_post(id, check_author=True):
+    post = get_db().execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
+
+    if post is None:
+        abort(404, "Post id {0} doesn't exist.".format(id))
+
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+
+    return post
+
+@bp.route('/<int:id>/delete', methods=('GET', 'POST'))
+@login_required
+def delete(id):
+    print(id)
+    print("test")
+    post = get_post(id)
+
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.commit()
+    
+    return redirect(url_for('post.index'))
